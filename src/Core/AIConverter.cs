1e1101ea8aae0b18637e6973eb8847a4a1f01906
dea1j <jdayo2012@gmail.com>
2025-03-03 16:25:14 +0100

    intial

---
+++ src/Core/AIConverter.cs
@@ -0,0 +1,29 @@
+﻿using OllamaSharp;
+using System.Threading.Tasks;
+
+namespace CodeMigrator.Core
+{
+    public class AIConverter
+    {
+        private readonly OllamaApiClient _ollama;
+
+        public AIConverter()
+        {
+            _ollama = new OllamaApiClient("http://localhost:11434");
+            _ollama.SelectedModel = "codellama";
+        }
+
+        public async Task<string> ConvertToBlazorAsync(string winFormsCode)
+        {
+            var prompt = $@"
+                Convert this WinForms code to a Blazor Razor component. 
+                Use modern syntax and components.
+                Input code:
+                {winFormsCode}
+            ";
+
+            var response = await _ollama.Generate(prompt);
+            return response.Response;
+        }
+    }
+}
---
+++ src/Core/WinFormsParser.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public class WinFormsParser
+    {
+        public List<ControlInfo> ParseControls(string code)
+        {
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var root = tree.GetRoot();
+            var controls = new List<ControlInfo>();
+
+            // Find all control declarations (e.g., "this.button1 = new Button();")
+            var assignments = root.DescendantNodes()
+                .OfType<AssignmentExpressionSyntax>()
+                .Where(a => a.Right is ObjectCreationExpressionSyntax);
+
+            foreach (var assignment in assignments)
+            {
+                var controlName = ((IdentifierNameSyntax)assignment.Left).Identifier.Text;
+                var controlType = ((ObjectCreationExpressionSyntax)assignment.Right).Type.ToString();
+                controls.Add(new ControlInfo { Name = controlName, Type = controlType });
+            }
+
+            return controls;
+        }
+    }
+
+    public class ControlInfo
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
